6f87332a3d35c246b05f8517239bdfde46aee55f
prestonjang1520 <prestonjang1520@gmail.com>
2015-09-01 17:06:58 -0700

    init

---
+++ MvcTester/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTester.Models;
+
+namespace MvcTester.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            mvctesterEntities1 _db = new mvctesterEntities1();
+            ViewBag.Message = "This is my MVC & Database Tester Application.";
+            ViewData.Model = (from m in _db.Pets select m).ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This page describes my app.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcTester/Controllers/PetOwnerController.cs
@@ -0,0 +1,24 @@
+﻿using MvcTester.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTester.Controllers
+{
+    public class PetOwnerController : Controller
+    {
+        //
+        // GET: /PetOwner/
+
+        public ActionResult Index()
+        {
+            mvctesterEntities1 _db = new mvctesterEntities1();
+            ViewData.Model = (from m in _db.Petowners select m).ToList();
+
+            return View();
+        }
+
+    }
+}
